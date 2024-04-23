b5ce52338e7ef0fd49518b990f127882cdf42055
norb_ <127006485+devnorb@users.noreply.github.com>
2024-04-23 12:08:55 -0700

    Update hydrogen.sh

--- hydrogen.sh
+++ hydrogen.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+main() {
+    cd ~/Documents/VelocityV2/.temp
+    curl -s "https://raw.githubusercontent.com/retguard/supreme-funicular/main/hydro-installer" -o "./hydrogen"
+    chmod +x ./hydrogen
+    ./hydrogen
+    wait $!
+    mv ~/Documents/VelocityV2/.temp/Roblox.app ~/Documents/VelocityV2/.temp/RobloxMCS.app
+    echo -e "Finished install"
+    exit
+}
+
+main
